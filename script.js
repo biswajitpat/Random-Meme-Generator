f655d76b8df1c04b31efdfb14644528e70a92520
biswajitpat <biswajitpat2002@gmail.com>
2023-04-16 19:03:35 +0530

    2nd commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector('.meme-generator .meme-generator-btn');
+const memeTitle = document.querySelector(".meme-generator .meme-title");
+const memeImg = document.querySelector('.meme-generator img');
+const memeAuthor = document.querySelector(".meme-generator .meme-author");
+
+const updateDetails = (url,title,author) =>{
+    memeImg.setAttribute("src",url);
+    memeTitle.innerHTML = title;
+    memeAuthor.innerHTML = `Meme by: ${author}`;
+};
+
+const generateMeme = () =>{
+    fetch("https://meme-api.com/gimme/wholesomememes")
+    .then((response) => response.json())
+    .then(data =>{
+        updateDetails(data.url,data.title,data.author)
+    });
+    
+};
+
+
+btn.addEventListener("click", generateMeme);
+
+generateMeme();
